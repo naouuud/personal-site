545e29da6b3189024831cb71d15df66da48fa617
Nour Aoude <naoude@gmail.com>
2024-02-11 16:48:07 +0200

    Added background image

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "img-1": "url('/public/images/bg-1.jpg')",
+        "img-2": "url('/public/images/bg-2.jpg')",
+      },
+    },
     fontFamily: {
       serif: ["IBM Plex Serif"],
       sans: ["Zilla Slab"],
